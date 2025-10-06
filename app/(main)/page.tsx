333ec040f53e89f69b579a79c04a21af5e84c3c0
kanoliban <kanoliban@gmail.com>
2025-10-05 19:53:58 -0500

    fix: properly lower Current Shift and Care Activities with margin-top
    - Removed incorrect absolute positioning that caused overlapping
    - Added mt-[calc(120px+24px)] to push bottom cards down by the extra Schedule height
    - Current Shift and Care Activities now positioned lower to align with Schedule bottom
    - Coverage Gaps and Care Team Status remain at top
    - Bottom edges now properly align horizontally across all three columns

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -65,21 +65,21 @@ export default function PageHome() {
       <div className='flex flex-col gap-6 px-4 pb-6 lg:px-8 lg:pt-1'>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 animate-float items-stretch'>
           {/* Left Column: Coverage Gaps, Current Shift */}
-          <div className='relative'>
+          <div className='flex flex-col gap-6'>
             <div className='h-80'>
               <WidgetTimeOff />
             </div>
-            <div className='h-80 absolute bottom-0 left-0 right-0'>
+            <div className='h-80 mt-[calc(120px+24px)]'>
               <WidgetCurrentProject />
             </div>
           </div>
           
           {/* Middle Column: Care Team Status, Care Activities */}
-          <div className='relative'>
+          <div className='flex flex-col gap-6'>
             <div className='h-80'>
               <WidgetStatusTracker />
             </div>
-            <div className='h-80 absolute bottom-0 left-0 right-0'>
+            <div className='h-80 mt-[calc(120px+24px)]'>
               <WidgetNotes />
             </div>
           </div>
